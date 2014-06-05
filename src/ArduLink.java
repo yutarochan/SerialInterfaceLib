2b4ef8af04c479bf38d3190c7cc025c85c9ec891
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2014-06-05 06:49:20 -0400

    Initial Commit

---
+++ src/ArduLink.java
@@ -0,0 +1,13 @@
+import org.zu.ardulink.Link;
+import org.zu.ardulink.gui.SerialConnectionPanel;
+
+public class ArduLink {
+
+	private static SerialConnectionPanel serialConnectionPanel;
+	
+	public static void main(String[] args) {
+		String connectionPort = (String) Link.getDefaultInstance().getPortList().get(0);
+		int baudRate = 9600;
+		boolean connected = Link.getDefaultInstance().connect(connectionPort, baudRate);
+	}
+}

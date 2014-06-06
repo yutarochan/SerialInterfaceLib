2be945fe13f921f018618bfc918326d2f392a6bd
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2014-06-06 03:22:22 -0400

    Added Device Port Recognition

--- src/ArduLink.java
+++ src/ArduLink.java
@@ -1,14 +1,39 @@
 import org.zu.ardulink.Link;
 import org.zu.ardulink.gui.SerialConnectionPanel;
 
+/*
+ * To make this work:
+ * - Must be in root
+ * - Add the device to the corresponding system under dev!!
+ * Resource: http://forum.arduino.cc/index.php/topic,125852.0.html
+ */
+
 public class ArduLink {
 
 	private static SerialConnectionPanel serialConnectionPanel;
 	
 	public static void main(String[] args) {
-		String connectionPort = (String) Link.getDefaultInstance().getPortList().get(0);
-		int baudRate = 9600;
-		
-		
+		String port = Link.getDefaultInstance().getPortList().get(0);
+		final String keyPort = port; 
+				
+		Thread t = new Thread(new Runnable() {
+
+			public void run() {
+				while (true) {
+					if (keyPort.equals(Link.getDefaultInstance().getPortList().get(0)))
+						System.out.println(keyPort + " LIKNED!");
+					else System.out.println(keyPort + " UNLINK!");
+					
+					try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				
+			}
+		});
+		t.start();
 	}
 }
---
+++ src/CheckDeviceLink.java
@@ -0,0 +1,9 @@
+public class CheckDeviceLink implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
